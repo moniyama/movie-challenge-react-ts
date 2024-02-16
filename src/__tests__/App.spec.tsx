835bb9b3e3b48b3e670b465e22f3fd0eb157ab7c
Monica <monicasayama@gmail.com>
2024-02-16 18:00:16 -0300

    add App DOM testing

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,5 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import App from "../components/App";
+
 describe("AppComponent", () => {
 	test("demo", () => {
 		expect(true).toBe(true);
+	});
+	test("Renders the main page", () => {
+		render(<App />);
+		expect(screen.queryByText("Vite + React")).toBeTruthy();
 	});
 });
