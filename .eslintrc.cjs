2328f2f9d20d61c0a34921cd8080c300210c35d7
Monica <monicasayama@gmail.com>
2024-02-19 14:34:44 -0300

    some eslint rules & pretest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,11 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +17,17 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "linebreak-style": ["error", "unix"],
+    "consistent-return": 2,
+    "indent": [1, 2],
+    "no-else-return": 1,
+    "semi": [1, "always"],
+    "space-unary-ops": 2
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
 }
--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -2,13 +2,13 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import App from "../components/App";
 
 describe("AppComponent", () => {
-	test("demo", () => {
-		expect(true).toBe(true);
-	});
-	test("Renders the main page", () => {
-		render(<App />);
-		fireEvent.click(screen.getByText("count is 0"));
-		expect(screen.queryByText("Vite + React")).toBeTruthy();
-		expect(screen.queryByText("count is 1")).toBeTruthy();
-	});
+  test("demo", () => {
+    expect(true).toBe(true);
+  });
+  test("Renders the main page", () => {
+    render(<App />);
+    fireEvent.click(screen.getByText("count is 0"));
+    expect(screen.queryByText("Vite + React")).toBeTruthy();
+    expect(screen.queryByText("count is 1")).toBeTruthy();
+  });
 });
