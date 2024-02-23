1dc72bae9f21f72bcfb0413395c56bc22d91bf9b
Monica <monicasayama@gmail.com>
2024-02-23 17:19:16 -0300

    add formatmovie vote_average test

--- src/utils/transformers.spec.ts
+++ src/utils/transformers.spec.ts
@@ -93,5 +93,7 @@ describe("formatMovie utils function", () => {
   it("returns object transformed", () => {
     expect(formatMovie(filmesAPI[0]).title).toBe("Badland Hunters");
     expect(formatMovie(filmesAPI[0]).release_year).toBe(2024);
+    expect(formatMovie(filmesAPI[0]).vote_average).toBeGreaterThanOrEqual(0);
+    expect(formatMovie(filmesAPI[0]).vote_average).toBeLessThanOrEqual(10);
   });
 });
