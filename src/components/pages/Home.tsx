26ed562c84bfd10d509a9d9ee500f8f11acc358a
moniyama <monicasayama@gmail.com>
2024-03-07 23:38:07 -0300

    adapt home to useQuery hook

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,46 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import HTTPService from "../../services/APIService";
-import { IMovie } from "../../models/Movie";
 import MovieList from "../MovieList/MovieList";
 import Pagination from "../Pagination/Pagination";
+import { useQuery } from '@tanstack/react-query'
 
 function Home() {
-  const [movies, setMovies] = useState<IMovie[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(false);
   const [page, setPage] = useState({ current: 1, total: 1 });
 
-  async function getMovies() {
-    try {
-      setIsLoading(true);
-      const result = await HTTPService.getMovies({
-        filters: {
-          page: 1,
-        },
-      });
-      setPage({
-        current: result.metaData.pagination.currentPage,
-        total: result.metaData.pagination.totalPages,
-      });
-      setMovies(result.movies);
-      setIsLoading(false);
-    } catch (err) {
-      setIsLoading(false);
-      setError(true);
-    }
+  async function fetchMovies() {
+    return await HTTPService.getMovies({
+      filters: {
+        page: page.current,
+      },
+    });
   }
 
-  useEffect(() => {
-    getMovies();
-  }, []);
+  const { isPending, isError, data, error } = useQuery({ queryKey: ['todos', page], queryFn: fetchMovies })
 
   return (
     <>
-      {isLoading ? <p>carregando...</p> : ""}
-      {error && <p>Falha na requisição.. Tente novamente mais tarde</p>}
-      {!isLoading && (
+      {isPending ? <p>carregando...</p> : ""}
+      {isError && <p>{error.message}</p>}
+      {data && (
         <>
-          <MovieList movies={movies} />
+          <MovieList movies={data.movies} />
           <Pagination
             currentPage={page.current}
             onSelectPage={setPage}
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider, } from '@tanstack/react-query'
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -14,8 +15,12 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </QueryClientProvider>
 );
