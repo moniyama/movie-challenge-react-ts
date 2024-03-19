a81be40f454281766a32f5676c0898b072ee3bab
Sergio Sinuco <ssinuco@gmail.com>
2024-03-18 20:46:51 -0500

    Task 4.4

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -2,12 +2,13 @@ import "./MovieCard.css";
 import { IMovie } from "../../models/Movie";
 
 function MovieCard({ movie }: { movie: IMovie }) {
-  const { title, releaseYear, poster } = movie;
+  const { title, releaseYear, poster, genres } = movie;
   return (
     <li>
       <h1>{title}</h1>
       <img src={poster} alt={`poster do filme ${title}`} />
       <p>{releaseYear}</p>
+      <p>{genres.join('-')}</p>
     </li>
   );
 }
