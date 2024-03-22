35142e3fa1b38bfe3d351190fe1a128af7877576
moniyama <monicasayama@gmail.com>
2024-03-22 17:13:41 -0300

    fix when genreId is null

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -17,7 +17,12 @@ const HTTPService = {
   ): Promise<IPaginationResponse> => {
     const { filters } = props;
 
-    const query = `?page=${filters.page}?sort_by=${filters.sortBy}&with_genres=${filters.genreId}`;
+    let query = ""
+    if (!filters.genreId) {
+      query = `?page=${filters.page}?sort_by=${filters.sortBy}`;
+    } else {
+      query = `?page=${filters.page}?sort_by=${filters.sortBy}&with_genres=${filters.genreId}`;
+    }
     const url = `https://api.themoviedb.org/3/discover/movie${query}`;
 
     return fetch(url, options)
