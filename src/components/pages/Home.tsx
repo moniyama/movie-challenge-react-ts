e714d0678835374c1133236b60f59b4bd815cb36
moniyama <monicasayama@gmail.com>
2024-03-23 12:15:32 -0300

    keep select option as reloading page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -12,23 +12,25 @@ import ListOptions, { IMovieLabel } from "../ListOptions/ListOptions";
 
 function Home() {
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const pageParam = Number(searchParams.get("page"))
+  const genreIdParam = Number(searchParams.get("genre"))
+
   const [totalPages, setTotalPages] = useState<number>(0);
-  const [currentPage, setCurrentPage] = useState<number>(
-    Number(searchParams.get("page")) || 1,
-  );
+  const [currentPage, setCurrentPage] = useState<number>(pageParam || 1);
 
   const [movies, setMovies] = useState<IMovie[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<boolean>(false);
   const [genresMap, setGenresMap] = useState<Map<number, string>>(new Map());
   const [filterGenre, setFilterGenre] = useState<number | null>(
-    Number(searchParams.get("genre")) || null,
+    genreIdParam || null,
   );
   // const [sortBy, setSortBy] = useState<string | null>(null);
-  const [selectedOption, setSelectedOption] = useState<IMovieLabel | null>(
-    null,
-  );
   const [genreOptions, setGenreOptions] = useState<IMovieLabel[]>([]);
+  const [selectedOption, setSelectedOption] = useState<IMovieLabel | null>(
+    genreOptions.find(item => item.value === filterGenre) || null,
+  );
 
   async function getMovies(page: number, map: Map<number, string>) {
     setError(false);
@@ -76,6 +78,11 @@ function Home() {
   }
 
   useEffect(() => {
+    const find = genreOptions.find(item => item.value === filterGenre) || null
+    setSelectedOption(find)
+  }, [filterGenre, genreOptions])
+
+  useEffect(() => {
     checkURL(currentPage);
   }, [genresMap]);
 
@@ -96,19 +103,10 @@ function Home() {
             selectedOption={selectedOption}
             onChange={(id: number | null) => {
               resetMovie();
-              if (!id) {
-                setSelectedOption(null);
-              } else {
-                const label =
-                  genreOptions.find((item) => item.value === id) || null;
-                setSelectedOption(label);
-                setSelectedOption(label);
-              }
               setFilterGenre(id);
             }}
             onClear={() => {
               resetMovie();
-              setSelectedOption(null);
               setFilterGenre(null);
             }}
           />
