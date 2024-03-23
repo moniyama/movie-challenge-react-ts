d13cf96fac6503bd617eec0ffca8ab0ccb688121
moniyama <monicasayama@gmail.com>
2024-03-23 13:22:43 -0300

    fix list options tests

--- src/__tests__/ListOptions.spec.tsx
+++ src/__tests__/ListOptions.spec.tsx
@@ -2,24 +2,21 @@ import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import ListOptions from "../components/ListOptions/ListOptions";
 import { genderOptions, selectedGenderOptions } from "../__mocks__/mocks";
 
-afterEach(() => cleanup);
+afterEach(cleanup);
+
 const onChangeMock = jest.fn();
 const onCleanMock = jest.fn();
 
-beforeEach(() => {
-  render(
-    <ListOptions
-      options={genderOptions}
-      selectedOption={selectedGenderOptions}
-      onChange={(e: number | null) => onChangeMock(e)}
-      onClear={onCleanMock}
-    />,
-  );
-});
-
 describe("List options Component", () => {
   it("render component", async () => {
-    const { findAllByRole, getByText } = screen;
+    const { findAllByRole, getByText } = render(
+      <ListOptions
+        options={genderOptions}
+        selectedOption={selectedGenderOptions}
+        onChange={(e: number | null) => onChangeMock(e)}
+        onClear={onCleanMock}
+      />,
+    );
 
     const selects = await findAllByRole("option");
     expect(selects).toHaveLength(4);
@@ -28,17 +25,47 @@ describe("List options Component", () => {
     expect((selects[2] as HTMLOptionElement).selected).toBeFalsy();
   });
 
+  it("render component when selected option is null", async () => {
+    const { findAllByRole } = render(
+      <ListOptions
+        options={genderOptions}
+        selectedOption={null}
+        onChange={(e: number | null) => onChangeMock(e)}
+        onClear={onCleanMock}
+      />,
+    );
+
+    const selects = await findAllByRole("option");
+    expect(selects).toHaveLength(4);
+    expect((selects[0] as HTMLOptionElement).selected).toBeTruthy();
+    expect((selects[1] as HTMLOptionElement).selected).toBeFalsy();
+    expect((selects[2] as HTMLOptionElement).selected).toBeFalsy();
+  });
+
   it("on change is fired when selected value is 28", async () => {
-    const { findByRole } = screen;
+    const { findByRole } = render(
+      <ListOptions
+        options={genderOptions}
+        selectedOption={selectedGenderOptions}
+        onChange={(e: number | null) => onChangeMock(e)}
+        onClear={onCleanMock}
+      />,
+    );
 
     const selects = await findByRole("combobox");
-    screen.debug(selects);
     fireEvent.change(selects, { target: { value: "28" } });
     expect(onChangeMock).toHaveBeenCalledWith(28);
   });
 
   it("on change is fired when selected value is default", async () => {
-    const { findByRole } = screen;
+    const { findByRole } = render(
+      <ListOptions
+        options={genderOptions}
+        selectedOption={selectedGenderOptions}
+        onChange={(e: number | null) => onChangeMock(e)}
+        onClear={onCleanMock}
+      />,
+    );
 
     const selects = await findByRole("combobox");
     screen.debug(selects);
@@ -47,7 +74,14 @@ describe("List options Component", () => {
   });
 
   it("clean button is fired", async () => {
-    const { findByRole } = screen;
+    const { findByRole } = render(
+      <ListOptions
+        options={genderOptions}
+        selectedOption={selectedGenderOptions}
+        onChange={(e: number | null) => onChangeMock(e)}
+        onClear={onCleanMock}
+      />,
+    );
     const button = await findByRole("button");
     fireEvent.click(button);
     expect(onCleanMock).toHaveBeenCalled();
