7ec4cc038a50b074395c92989da70a01467e796a
moniyama <monicasayama@gmail.com>
2024-03-24 01:00:33 -0300

    finish pagination test

--- src/__tests__/Pagination.spec.tsx
+++ src/__tests__/Pagination.spec.tsx
@@ -49,7 +49,7 @@ describe("Pagination component", () => {
     });
   });
 
-  describe("is at  but one page", () => {
+  describe("is at but one page", () => {
     it("render 13 buttons", async () => {
       render(
         <Pagination
@@ -83,6 +83,45 @@ describe("Pagination component", () => {
       expect(
         btns.filter((item) => item.textContent === "Proximo")[0],
       ).toBeDisabled();
+    });
+  });
+
+  describe("current pages is lower than displayed pages", () => {
+    it("render displays pages previous of current", async () => {
+      render(
+        <Pagination
+          currentPage={-5}
+          totalPages={totalPages}
+          onSelectPage={handleClick}
+        />,
+      );
+      const btns = await screen.findAllByRole("button");
+      expect(btns).toHaveLength(14);
+      expect(btns.filter((item) => item.textContent === "-5")[0]).toHaveClass(
+        "current-page",
+      );
+    });
+  });
+
+  describe("has More Displayed Pages Than Total Pages on init: total page is lower than 10", () => {
+    it("renders only needed butons", async () => {
+      render(
+        <Pagination
+          currentPage={3}
+          totalPages={5}
+          onSelectPage={handleClick}
+        />,
+      );
+      const btns = await screen.findAllByRole("button");
+      const nextBtn = await screen.findByText("Proximo");
+      const previousBtn = await screen.findByText("Anterior");
+
+      expect(btns).toHaveLength(7);
+      expect(btns.filter((item) => item.textContent === "3")[0]).toHaveClass(
+        "current-page",
+      );
+      expect(nextBtn).not.toBeDisabled();
+      expect(previousBtn).not.toBeDisabled();
     });
   });
 
